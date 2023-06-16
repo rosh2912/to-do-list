e49be41f9d51e13d833772b72ca9d325c32c787f
rosh2912 <limbaniroshan603@gmail.com>
2023-06-16 19:45:28 +0530

    Updates

---
+++ precache-manifest.d3f9e3c0c53c967b6ed11afa850f7c1e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e0e358edf4970c2275bbfd6543ba64",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "a2d12905c0b569165bba",
+    "url": "/to-do-list/static/js/2.61ae90a2.chunk.js"
+  },
+  {
+    "revision": "0e5c5644d517acb89193",
+    "url": "/to-do-list/static/js/main.6067bbec.chunk.js"
+  },
+  {
+    "revision": "0f6c7dce86e76ba96de6",
+    "url": "/to-do-list/static/js/runtime-main.e768a3cb.js"
+  }
+]);
